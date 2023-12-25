5e11a1f562b78caf28f51395ffd5bca07910ce8d
rekerok05 <rekerok05@gmail.com>
2023-12-26 00:32:56 +0300

    add deploy settings

--- module_settings.py
+++ module_settings.py
@@ -173,6 +173,22 @@ class MERKLY_SETTINGS:
     SLEEP = (100, 150)
 
 
+class DEPLOY_SETTINGS:
+    params = [
+        {
+            "network": Networks.polygon,
+            "contracts": [
+                {
+                    "name": "merkly",
+                    "bytecode": "0x60806040526000805461ffff1916905534801561001b57600080fd5b5060fb8061002a6000396000f3fe6080604052348015600f57600080fd5b506004361060325760003560e01c80630c55699c146037578063b49004e914605b575b600080fd5b60005460449061ffff1681565b60405161ffff909116815260200160405180910390f35b60616063565b005b60008054600191908190607a90849061ffff166096565b92506101000a81548161ffff021916908361ffff160217905550565b61ffff81811683821601908082111560be57634e487b7160e01b600052601160045260246000fd5b509291505056fea2646970667358221220666c87ec501268817295a4ca1fc6e3859faf241f38dd688f145135970920009264736f6c63430008120033",
+                }
+            ],
+            "count": (1, 1),
+        }
+    ]
+    SLEEP = (100, 200)
+
+
 ### NOT CHANGE ###
 async def okx_withdrawer():
     await OKX.withdraw_use_database(OKX_settings)
@@ -200,3 +216,7 @@ async def merkly():
 
 async def merkly_check_comission():
     await Merkly.get_fees(from_chains=MERKLY_SETTINGS.CHECK_COMISSION_NETWORKS)
+
+
+async def deploy_contracts():
+    await Deployer.deploy_with_database(settings=DEPLOY_SETTINGS)
