ca5bbc31e1cf5b27e4aef7a663512cd43eb333fd
rekerok05 <rekerok05@gmail.com>
2023-12-28 01:58:11 +0300

    fix 1inch

--- modules/swaps/inch_swap.py
+++ modules/swaps/inch_swap.py
@@ -1,12 +1,14 @@
 import time
-import eth_utils
-import settings
 import utils
+import config
+import settings
+import eth_utils
 from typing import Union
+from loguru import logger
 from utils import TYPES_OF_TRANSACTION
 from utils import Token_Amount, Token_Info
+from utils.enums import RESULT_TRANSACTION
 from modules.web3Swapper import Web3Swapper
-from utils.enums import NETWORK_FIELDS
 
 
 class InchSwap(Web3Swapper):
@@ -30,19 +32,18 @@ class InchSwap(Web3Swapper):
             slippage=slippage,
         )
 
-    async def _get_contract_address(
-        self,
-    ):
-        url = f"https://api.1inch.dev/swap/v5.2/{await self.acc.w3.eth.chain_id}/approve/spender"
-
+    async def _get_contract_address(self, chain_id):
+        url = f"https://api.1inch.dev/swap/v5.2/{chain_id}/approve/spender"
         headers = {
             "Authorization": f"Bearer {settings.INCH_SWAP_KEY}",
             "accept": "application/json",
         }
         response = await utils.aiohttp.get_json_aiohttp(url=url, headers=headers)
+        if not response:
+            return None
         return response.get("address")
 
-    async def _get_quote(
+    async def _get_swap_data(
         self, from_token: Token_Info, to_token: Token_Info, amount: Token_Amount
     ):
         url = f"https://api.1inch.dev/swap/v5.2/{await self.acc.w3.eth.chain_id}/swap"
@@ -50,13 +51,12 @@ class InchSwap(Web3Swapper):
             "Authorization": f"Bearer {settings.INCH_SWAP_KEY}",
             "accept": "application/json",
         }
-
         params = {
             "src": from_token.address,
             "dst": to_token.address,
             "amount": amount.WEI,
             "from": self.acc.address,
-            "slippage": 1,
+            "slippage": 5,
         }
 
         response = await utils.aiohttp.get_json_aiohttp(
@@ -65,42 +65,52 @@ class InchSwap(Web3Swapper):
             params=params,
         )
 
+        if not response:
+            return None
         return response["tx"]["data"]
 
+    # https://docs.1inch.io/
     async def _perform_swap(
         self,
         amount_to_send: Token_Amount,
         from_token: Token_Info,
         to_token: Token_Info,
     ):
-        contract_address = eth_utils.address.to_checksum_address(
-            await self._get_contract_address()
-        )
         from_token, to_token = await Token_Info.to_native_token(
-            from_token=from_token,
-            to_token=to_token,
+            from_token=from_token, to_token=to_token
         )
-
-        if from_token.symbol != self.acc.network.get(NETWORK_FIELDS.NATIVE_TOKEN):
+        chain_id = await self.acc.w3.eth.chain_id
+        contract_address = await self._get_contract_address(chain_id=chain_id)
+        if contract_address:
+            contract_address = eth_utils.address.to_checksum_address(contract_address)
+        else:
+            logger.warning("CHECK APIKEY")
+            return RESULT_TRANSACTION.FAIL
+        logger.debug("NEED SLEEP 10 SEC")
+        time.sleep(10)
+        if from_token.address != config.GENERAL.NATIVE_TOKEN.value:
             await self.acc.approve(
                 token_address=from_token.address,
                 spender=contract_address,
                 amount=amount_to_send,
             )
-
-        data = await self._get_quote(
+        data = await self._get_swap_data(
             from_token=from_token, to_token=to_token, amount=amount_to_send
         )
+        if not data:
+            logger.error("FAIL GET DATA FOR SWAP OR NOT BALANCE")
+            return RESULT_TRANSACTION.FAIL
 
-        value = (
-            amount_to_send.WEI
-            if from_token.symbol == self.acc.network.get(NETWORK_FIELDS.NATIVE_TOKEN)
-            else None
+        value, value_approve = await Web3Swapper._get_value_and_allowance(
+            amount=amount_to_send,
+            from_native_token=True
+            if from_token.address == config.GENERAL.NATIVE_TOKEN.value
+            else False,
         )
         return await self._send_swap_transaction(
             data=data,
             from_token=from_token,
             to_address=contract_address,
-            value_approove=None,
+            value_approve=None,
             value=value,
         )
