8137ce8d2404565f83d68b64cd7d021cc2a930c1
rekerok05 <rekerok05@gmail.com>
2024-10-16 00:26:39 +0300

    fix warmup

--- modules/swaps/warmup_swaps.py
+++ modules/swaps/warmup_swaps.py
@@ -127,49 +127,54 @@ class WarmUPSwaps:
         random.shuffle(database)
         counter = 1
         for data in database:
-            logger.info(f"OPERATION {counter}/{len(database)}")
-            acc = Account(
-                private_key=data.get("private_key"),
-                network=data.get("network"),
-            )
-            logger.info(f"WALLET {acc.address}")
-            if settings.USE_MAX_BALANCE:
-                pair_tokens = await WarmUPSwaps._get_pair_with_max_for_swap(
-                    tokens=data.get("tokens"), acc=acc
+            try:
+                logger.info(f"OPERATION {counter}/{len(database)}")
+                acc = Account(
+                    private_key=data.get("private_key"),
+                    network=data.get("network"),
                 )
-            else:
-                pair_tokens = await WarmUPSwaps._get_random_pair_for_swap(
-                    tokens=data.get("tokens"), acc=acc
+                logger.info(f"WALLET {acc.address}")
+                if settings.USE_MAX_BALANCE:
+                    pair_tokens = await WarmUPSwaps._get_pair_with_max_for_swap(
+                        tokens=data.get("tokens"), acc=acc
+                    )
+                else:
+                    pair_tokens = await WarmUPSwaps._get_random_pair_for_swap(
+                        tokens=data.get("tokens"), acc=acc
+                    )
+                if pair_tokens[0] is None or pair_tokens[1] is None:
+                    logger.info(
+                        Account(
+                            private_key=data.get("private_key"),
+                            network=data.get("network"),
+                        ).address
+                    )
+                    logger.error(
+                        f"NOT PAIR FOR SWAP IN {data.get('dex')} {data.get('network').get(NETWORK_FIELDS.NAME)}"
+                    )
+                    counter += 1
+                    continue
+                dex: Web3Swapper = random.choice(data["dexs"])(
+                    private_key=data.get("private_key"),
+                    network=data.get("network"),
+                    type_transfer=TYPES_OF_TRANSACTION.PERCENT,
+                    value=pair_tokens[0].get(PARAMETR.VALUE),
+                    min_balance=pair_tokens[0].get(PARAMETR.MIN_BALANCE),
+                    max_balance=pair_tokens[0].get(PARAMETR.MAX_BALANCE),
+                    slippage=settings.SLIPPAGE,
                 )
-            if pair_tokens[0] is None or pair_tokens[1] is None:
-                logger.info(
-                    Account(
-                        private_key=data.get("private_key"),
-                        network=data.get("network"),
-                    ).address
+                # result = RESULT_TRANSACTION.FAIL
+                result = await dex.swap(
+                    from_token=pair_tokens[0].get(PARAMETR.TOKEN),
+                    to_token=pair_tokens[1].get(PARAMETR.TOKEN),
                 )
-                logger.error(
-                    f"NOT PAIR FOR SWAP IN {data.get('dex')} {data.get('network').get(NETWORK_FIELDS.NAME)}"
-                )
+                if result == RESULT_TRANSACTION.SUCCESS:
+                    await utils.time.sleep_view(settings.SLEEP)
+                else:
+                    await utils.time.sleep_view((10, 15))
+                logger.info("------------------------------------")
                 counter += 1
-                continue
-            dex: Web3Swapper = random.choice(data["dexs"])(
-                private_key=data.get("private_key"),
-                network=data.get("network"),
-                type_transfer=TYPES_OF_TRANSACTION.PERCENT,
-                value=pair_tokens[0].get(PARAMETR.VALUE),
-                min_balance=pair_tokens[0].get(PARAMETR.MIN_BALANCE),
-                max_balance=pair_tokens[0].get(PARAMETR.MAX_BALANCE),
-                slippage=settings.SLIPPAGE,
-            )
-            # result = RESULT_TRANSACTION.FAIL
-            result = await dex.swap(
-                from_token=pair_tokens[0].get(PARAMETR.TOKEN),
-                to_token=pair_tokens[1].get(PARAMETR.TOKEN),
-            )
-            if result == RESULT_TRANSACTION.SUCCESS:
-                await utils.time.sleep_view(settings.SLEEP)
-            else:
+            except Exception as error:
+                logger.error(error)
+                counter += 1
                 await utils.time.sleep_view((10, 15))
-            logger.info("------------------------------------")
-            counter += 1
